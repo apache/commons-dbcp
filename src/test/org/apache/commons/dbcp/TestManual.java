f5193b12ddab1c566fdaf09a0a6bf7db4de84a27
Rodney Waldhoff <rwaldhoff@apache.org>
2002-03-16 13:29:49 +0000

    convert to assertTrue() from assert()
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@131868 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/dbcp/TestManual.java
+++ src/test/org/apache/commons/dbcp/TestManual.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/test/org/apache/commons/dbcp/TestManual.java,v 1.1 2001/04/14 17:16:25 rwaldhoff Exp $
- * $Revision: 1.1 $
- * $Date: 2001/04/14 17:16:25 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/test/org/apache/commons/dbcp/TestManual.java,v 1.2 2002/03/16 13:29:49 rwaldhoff Exp $
+ * $Revision: 1.2 $
+ * $Date: 2002/03/16 13:29:49 $
  *
  * ====================================================================
  *
@@ -68,7 +68,7 @@ import org.apache.commons.pool.impl.*;
 
 /**
  * @author Rodney Waldhoff
- * @version $Id: TestManual.java,v 1.1 2001/04/14 17:16:25 rwaldhoff Exp $
+ * @version $Id: TestManual.java,v 1.2 2002/03/16 13:29:49 rwaldhoff Exp $
  */
 public class TestManual extends TestCase {
     public TestManual(String testName) {
@@ -96,12 +96,12 @@ public class TestManual extends TestCase {
 
     public void testSimple() throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assert(null != conn);
+        assertTrue(null != conn);
         PreparedStatement stmt = conn.prepareStatement("select * from dual");
-        assert(null != stmt);
+        assertTrue(null != stmt);
         ResultSet rset = stmt.executeQuery();
-        assert(null != rset);
-        assert(rset.next());
+        assertTrue(null != rset);
+        assertTrue(rset.next());
         rset.close();
         stmt.close();
         conn.close();
@@ -109,22 +109,22 @@ public class TestManual extends TestCase {
 
     public void testSimple2() throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assert(null != conn);
+        assertTrue(null != conn);
         {
             PreparedStatement stmt = conn.prepareStatement("select * from dual");
-            assert(null != stmt);
+            assertTrue(null != stmt);
             ResultSet rset = stmt.executeQuery();
-            assert(null != rset);
-            assert(rset.next());
+            assertTrue(null != rset);
+            assertTrue(rset.next());
             rset.close();
             stmt.close();
         }
         {
             PreparedStatement stmt = conn.prepareStatement("select * from dual");
-            assert(null != stmt);
+            assertTrue(null != stmt);
             ResultSet rset = stmt.executeQuery();
-            assert(null != rset);
-            assert(rset.next());
+            assertTrue(null != rset);
+            assertTrue(rset.next());
             rset.close();
             stmt.close();
         }
@@ -137,22 +137,22 @@ public class TestManual extends TestCase {
         }
 
         conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assert(null != conn);
+        assertTrue(null != conn);
         {
             PreparedStatement stmt = conn.prepareStatement("select * from dual");
-            assert(null != stmt);
+            assertTrue(null != stmt);
             ResultSet rset = stmt.executeQuery();
-            assert(null != rset);
-            assert(rset.next());
+            assertTrue(null != rset);
+            assertTrue(rset.next());
             rset.close();
             stmt.close();
         }
         {
             PreparedStatement stmt = conn.prepareStatement("select * from dual");
-            assert(null != stmt);
+            assertTrue(null != stmt);
             ResultSet rset = stmt.executeQuery();
-            assert(null != rset);
-            assert(rset.next());
+            assertTrue(null != rset);
+            assertTrue(rset.next());
             rset.close();
             stmt.close();
         }
@@ -162,10 +162,10 @@ public class TestManual extends TestCase {
 
     public void testPooling() throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assert(conn != null);
+        assertTrue(conn != null);
         Connection conn2 = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assert(conn2 != null);
-        assert(conn != conn2);
+        assertTrue(conn2 != null);
+        assertTrue(conn != conn2);
         conn2.close();
         conn.close();
         conn2 = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
