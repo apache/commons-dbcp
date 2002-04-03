4493b56efc27ff7c93218187ced697972713f7d1
Rodney Waldhoff <rwaldhoff@apache.org>
2002-04-03 12:41:56 +0000

    better test in testPooling, we don't care what order the pooled Connections are returned
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@131876 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/dbcp/TestManual.java
+++ src/test/org/apache/commons/dbcp/TestManual.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/test/org/apache/commons/dbcp/TestManual.java,v 1.4 2002/04/03 12:33:02 rwaldhoff Exp $
- * $Revision: 1.4 $
- * $Date: 2002/04/03 12:33:02 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/test/org/apache/commons/dbcp/TestManual.java,v 1.5 2002/04/03 12:41:56 rwaldhoff Exp $
+ * $Revision: 1.5 $
+ * $Date: 2002/04/03 12:41:56 $
  *
  * ====================================================================
  *
@@ -68,7 +68,7 @@ import org.apache.commons.pool.impl.*;
 
 /**
  * @author Rodney Waldhoff
- * @version $Id: TestManual.java,v 1.4 2002/04/03 12:33:02 rwaldhoff Exp $
+ * @version $Id: TestManual.java,v 1.5 2002/04/03 12:41:56 rwaldhoff Exp $
  */
 public class TestManual extends TestCase {
     public TestManual(String testName) {
@@ -150,8 +150,8 @@ public class TestManual extends TestCase {
             conn.createStatement();
             fail("Can't use closed connections");
         } catch(SQLException e) {
-            ; // expected
-        }
+            ;testSimpleve // expected
+        }testSimpleve
 
         conn = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
         assertTrue(null != conn);
@@ -185,7 +185,7 @@ public class TestManual extends TestCase {
         assertTrue(conn != conn2);
         conn2.close();
         conn.close();
-        conn2 = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
-        assertSame(conn,conn2);
+        Connection conn3 = DriverManager.getConnection("jdbc:apache:commons:dbcp:test");
+        assertTrue( conn3 == conn || conn3 == conn2 );
     }
 }
