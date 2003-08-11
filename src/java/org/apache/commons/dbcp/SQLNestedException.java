30ab5abcaa303f24b7fb0552c4e5e60719968d2a
Dirk Verbeeck <dirkv@apache.org>
2003-08-11 14:51:25 +0000

    A SQLException subclass containing another Throwable
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@131980 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/dbcp/SQLNestedException.java
@@ -0,0 +1,116 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/java/org/apache/commons/dbcp/SQLNestedException.java,v 1.1 2003/08/11 14:51:25 dirkv Exp $
+ * $Revision: 1.1 $
+ * $Date: 2003/08/11 14:51:25 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.commons.dbcp;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+/**
+ * A SQLException subclass containing another Throwable
+ * 
+ * @author Dirk Verbeeck
+ * @version $Id: SQLNestedException.java,v 1.1 2003/08/11 14:51:25 dirkv Exp $
+ */
+class SQLNestedException extends SQLException {
+
+    /**
+     * Holds the reference to the exception or error that caused
+     * this exception to be thrown.
+     */
+    private Throwable cause = null;
+
+    /**
+     * Constructs a new <code>SQLNestedException</code> with specified
+     * detail message and nested <code>Throwable</code>.
+     *
+     * @param msg    the error message
+     * @param cause  the exception or error that caused this exception to be
+     * thrown
+     */
+    public SQLNestedException(String msg, Throwable cause) {
+        super(msg);
+        if (cause == null) {
+            cause = new Exception("No cause");
+        }
+        this.cause = cause;
+    }
+    
+    public Throwable getCause() {
+        return this.cause;
+    }
+    public String getLocalizedMessage() {
+        return super.getLocalizedMessage() + ", cause: " + this.cause.getLocalizedMessage();
+    }
+    public String getMessage() {
+        return super.getMessage() + ", cause: " + this.cause.getMessage();
+    }
+    public void printStackTrace() {
+        this.cause.printStackTrace();
+    }
+    public void printStackTrace(PrintStream s) {
+        this.cause.printStackTrace(s);
+    }
+    public void printStackTrace(PrintWriter s) {
+        this.cause.printStackTrace(s);
+    }
+}
