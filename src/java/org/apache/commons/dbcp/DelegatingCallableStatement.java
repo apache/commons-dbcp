e5255dc0e405871cb488d609639fb0fde02f3db4
Dirk Verbeeck <dirkv@apache.org>
2003-12-26 17:03:35 +0000

    remove inherited methods
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@132126 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/DelegatingCallableStatement.java
+++ src/java/org/apache/commons/dbcp/DelegatingCallableStatement.java
@@ -1,7 +1,7 @@
 /*
  * $Source: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//dbcp/src/java/org/apache/commons/dbcp/DelegatingCallableStatement.java,v $
- * $Revision: 1.15 $
- * $Date: 2003/12/26 16:56:44 $
+ * $Revision: 1.16 $
+ * $Date: 2003/12/26 17:03:35 $
  *
  * ====================================================================
  *
@@ -73,11 +73,8 @@ import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.Array;
 import java.util.Calendar;
-import java.sql.ResultSet;
 import java.io.InputStream;
 import java.io.Reader;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLWarning;
 import java.sql.SQLException;
 
 /**
@@ -95,7 +92,7 @@ import java.sql.SQLException;
  * @author Glenn L. Nielsen
  * @author James House (<a href="mailto:james@interobjective.com">james@interobjective.com</a>)
  * @author Dirk Verbeeck
- * @version $Revision: 1.15 $ $Date: 2003/12/26 16:56:44 $
+ * @version $Revision: 1.16 $ $Date: 2003/12/26 17:03:35 $
  */
 public class DelegatingCallableStatement extends DelegatingPreparedStatement
         implements CallableStatement {
@@ -136,83 +133,6 @@ public class DelegatingCallableStatement extends DelegatingPreparedStatement
         _stmt = s;
     }
 
-    public ResultSet executeQuery(String sql) throws SQLException {
-        checkOpen();
-        return DelegatingResultSet.wrapResultSet(this,_stmt.executeQuery(sql));
-    }
-
-    public ResultSet getResultSet() throws SQLException {
-        checkOpen();
-        return DelegatingResultSet.wrapResultSet(this,_stmt.getResultSet());
-    }
-
-    public ResultSet executeQuery() throws SQLException {
-        checkOpen();
-        return DelegatingResultSet.wrapResultSet(this,_stmt.executeQuery());
-    }
-
-    public int executeUpdate(String sql) throws SQLException { checkOpen(); return _stmt.executeUpdate(sql);}
-    public int getMaxFieldSize() throws SQLException { checkOpen(); return _stmt.getMaxFieldSize();}
-    public void setMaxFieldSize(int max) throws SQLException { checkOpen();_stmt.setMaxFieldSize(max);}
-    public int getMaxRows() throws SQLException { checkOpen();return _stmt.getMaxRows();}
-    public void setMaxRows(int max) throws SQLException { checkOpen(); _stmt.setMaxRows(max);}
-    public void setEscapeProcessing(boolean enable) throws SQLException { checkOpen();_stmt.setEscapeProcessing(enable);}
-    public int getQueryTimeout() throws SQLException { checkOpen(); return _stmt.getQueryTimeout();}
-    public void setQueryTimeout(int seconds) throws SQLException { checkOpen(); _stmt.setQueryTimeout(seconds);}
-    public void cancel() throws SQLException { checkOpen(); _stmt.cancel();}
-    public SQLWarning getWarnings() throws SQLException { checkOpen(); return _stmt.getWarnings();}
-    public void clearWarnings() throws SQLException { checkOpen(); _stmt.clearWarnings();}
-    public void setCursorName(String name) throws SQLException { checkOpen(); _stmt.setCursorName(name);}
-    public boolean execute(String sql) throws SQLException { checkOpen(); return _stmt.execute(sql);}
-
-    public int getUpdateCount() throws SQLException { checkOpen(); return _stmt.getUpdateCount();}
-    public boolean getMoreResults() throws SQLException { checkOpen(); return _stmt.getMoreResults();}
-    public void setFetchDirection(int direction) throws SQLException { checkOpen(); _stmt.setFetchDirection(direction);}
-    public int getFetchDirection() throws SQLException { checkOpen(); return _stmt.getFetchDirection();}
-    public void setFetchSize(int rows) throws SQLException { checkOpen(); _stmt.setFetchSize(rows);}
-    public int getFetchSize() throws SQLException { checkOpen(); return _stmt.getFetchSize();}
-    public int getResultSetConcurrency() throws SQLException { checkOpen(); return _stmt.getResultSetConcurrency();}
-    public int getResultSetType() throws SQLException { checkOpen(); return _stmt.getResultSetType();}
-    public void addBatch(String sql) throws SQLException { checkOpen(); _stmt.addBatch(sql);}
-    public void clearBatch() throws SQLException { checkOpen(); _stmt.clearBatch();}
-    public int[] executeBatch() throws SQLException { checkOpen(); return _stmt.executeBatch();}
-
-    public int executeUpdate() throws SQLException { checkOpen(); return _stmt.executeUpdate();}
-    public void setNull(int parameterIndex, int sqlType) throws SQLException { checkOpen(); _stmt.setNull(parameterIndex,sqlType);}
-    public void setBoolean(int parameterIndex, boolean x) throws SQLException { checkOpen(); _stmt.setBoolean(parameterIndex,x);}
-    public void setByte(int parameterIndex, byte x) throws SQLException { checkOpen(); _stmt.setByte(parameterIndex,x);}
-    public void setShort(int parameterIndex, short x) throws SQLException { checkOpen(); _stmt.setShort(parameterIndex,x);}
-    public void setInt(int parameterIndex, int x) throws SQLException { checkOpen(); _stmt.setInt(parameterIndex,x);}
-    public void setLong(int parameterIndex, long x) throws SQLException { checkOpen(); _stmt.setLong(parameterIndex,x);}
-    public void setFloat(int parameterIndex, float x) throws SQLException { checkOpen(); _stmt.setFloat(parameterIndex,x);}
-    public void setDouble(int parameterIndex, double x) throws SQLException { checkOpen(); _stmt.setDouble(parameterIndex,x);}
-    public void setBigDecimal(int parameterIndex, BigDecimal x) throws SQLException { checkOpen(); _stmt.setBigDecimal(parameterIndex,x);}
-    public void setString(int parameterIndex, String x) throws SQLException { checkOpen(); _stmt.setString(parameterIndex,x);}
-    public void setBytes(int parameterIndex, byte[] x) throws SQLException { checkOpen(); _stmt.setBytes(parameterIndex,x);}
-    public void setDate(int parameterIndex, java.sql.Date x) throws SQLException { checkOpen(); _stmt.setDate(parameterIndex,x);}
-    public void setTime(int parameterIndex, java.sql.Time x) throws SQLException { checkOpen(); _stmt.setTime(parameterIndex,x);}
-    public void setTimestamp(int parameterIndex, java.sql.Timestamp x) throws SQLException { checkOpen(); _stmt.setTimestamp(parameterIndex,x);}
-    public void setAsciiStream(int parameterIndex, java.io.InputStream x, int length) throws SQLException { checkOpen(); _stmt.setAsciiStream(parameterIndex,x,length);}
-    /** @deprecated */
-    public void setUnicodeStream(int parameterIndex, java.io.InputStream x, int length) throws SQLException { checkOpen(); _stmt.setUnicodeStream(parameterIndex,x,length);}
-    public void setBinaryStream(int parameterIndex, java.io.InputStream x, int length) throws SQLException{ checkOpen(); _stmt.setBinaryStream(parameterIndex,x,length);}
-    public void clearParameters() throws SQLException { checkOpen(); _stmt.clearParameters();}
-    public void setObject(int parameterIndex, Object x, int targetSqlType, int scale) throws SQLException { checkOpen(); _stmt.setObject(parameterIndex, x, targetSqlType, scale);}
-    public void setObject(int parameterIndex, Object x, int targetSqlType) throws SQLException { checkOpen(); _stmt.setObject(parameterIndex, x, targetSqlType);}
-    public void setObject(int parameterIndex, Object x) throws SQLException { checkOpen(); _stmt.setObject(parameterIndex, x);}
-    public boolean execute() throws SQLException { checkOpen(); return _stmt.execute();}
-    public void addBatch() throws SQLException { checkOpen(); _stmt.addBatch();}
-    public void setCharacterStream(int parameterIndex, java.io.Reader reader, int length) throws SQLException { checkOpen(); _stmt.setCharacterStream(parameterIndex,reader,length);}
-    public void setRef(int i, Ref x) throws SQLException { checkOpen(); _stmt.setRef(i,x);}
-    public void setBlob(int i, Blob x) throws SQLException { checkOpen(); _stmt.setBlob(i,x);}
-    public void setClob(int i, Clob x) throws SQLException { checkOpen(); _stmt.setClob(i,x);}
-    public void setArray(int i, Array x) throws SQLException { checkOpen(); _stmt.setArray(i,x);}
-    public ResultSetMetaData getMetaData() throws SQLException { checkOpen(); return _stmt.getMetaData();}
-    public void setDate(int parameterIndex, java.sql.Date x, Calendar cal) throws SQLException { checkOpen(); _stmt.setDate(parameterIndex,x,cal);}
-    public void setTime(int parameterIndex, java.sql.Time x, Calendar cal) throws SQLException { checkOpen(); _stmt.setTime(parameterIndex,x,cal);}
-    public void setTimestamp(int parameterIndex, java.sql.Timestamp x, Calendar cal) throws SQLException { checkOpen(); _stmt.setTimestamp(parameterIndex,x,cal);}
-    public void setNull(int paramIndex, int sqlType, String typeName) throws SQLException { checkOpen(); _stmt.setNull(paramIndex,sqlType,typeName);}
-
     public void registerOutParameter(int parameterIndex, int sqlType) throws SQLException { checkOpen(); _stmt.registerOutParameter( parameterIndex,  sqlType);  }
     public void registerOutParameter(int parameterIndex, int sqlType, int scale) throws SQLException { checkOpen(); _stmt.registerOutParameter( parameterIndex,  sqlType,  scale);  }
     public boolean wasNull() throws SQLException { checkOpen(); return _stmt.wasNull();  }
@@ -246,68 +166,6 @@ public class DelegatingCallableStatement extends DelegatingPreparedStatement
     // Will be commented by the build process on a JDBC 2.0 system
 
 /* JDBC_3_ANT_KEY_BEGIN */
-
-    public boolean getMoreResults(int current) throws SQLException {
-        checkOpen();
-        return _stmt.getMoreResults(current);
-    }
-
-    public ResultSet getGeneratedKeys() throws SQLException {
-        checkOpen();
-        return _stmt.getGeneratedKeys();
-    }
-
-    public int executeUpdate(String sql, int autoGeneratedKeys)
-        throws SQLException {
-        checkOpen();
-        return _stmt.executeUpdate(sql, autoGeneratedKeys);
-    }
-
-    public int executeUpdate(String sql, int columnIndexes[])
-        throws SQLException {
-        checkOpen();
-        return _stmt.executeUpdate(sql, columnIndexes);
-    }
-
-    public int executeUpdate(String sql, String columnNames[])
-        throws SQLException {
-        checkOpen();
-        return _stmt.executeUpdate(sql, columnNames);
-    }
-
-    public boolean execute(String sql, int autoGeneratedKeys)
-        throws SQLException {
-        checkOpen();
-        return _stmt.execute(sql, autoGeneratedKeys);
-    }
-
-    public boolean execute(String sql, int columnIndexes[])
-        throws SQLException {
-        checkOpen();
-        return _stmt.execute(sql, columnIndexes);
-    }
-
-    public boolean execute(String sql, String columnNames[])
-        throws SQLException {
-        checkOpen();
-        return _stmt.execute(sql, columnNames);
-    }
-
-    public int getResultSetHoldability() throws SQLException {
-        checkOpen();
-        return _stmt.getResultSetHoldability();
-    }
-
-    public void setURL(int parameterIndex, java.net.URL x)
-        throws SQLException {
-        checkOpen();
-        _stmt.setURL(parameterIndex, x);
-    }
-
-    public java.sql.ParameterMetaData getParameterMetaData() throws SQLException {
-        checkOpen();
-        return _stmt.getParameterMetaData();
-    }
 
     public void registerOutParameter(String parameterName, int sqlType)
         throws SQLException {
