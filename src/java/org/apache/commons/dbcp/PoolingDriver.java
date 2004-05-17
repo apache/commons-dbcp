39fdf4ebf8042fb5bf8d85fe7b1acae40cb47c2b
Dirk Verbeeck <dirkv@apache.org>
2004-05-17 18:39:44 +0000

    Bugzilla Bug 28912: (PoolingDriver) Connection re-use conflates logical and physical connections - implement a PoolGuardConnectionWrapper for PoolingDriver - make error reporting behave like PoolingDataSource
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@132180 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/PoolingDriver.java
+++ src/java/org/apache/commons/dbcp/PoolingDriver.java
@@ -18,12 +18,19 @@ package org.apache.commons.dbcp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 import java.util.Set;
 
@@ -39,7 +46,7 @@ import org.xml.sax.SAXException;
  *
  * @author Rodney Waldhoff
  * @author Dirk Verbeeck
- * @version $Revision: 1.11 $ $Date: 2004/02/28 12:18:17 $
+ * @version $Revision: 1.12 $ $Date: 2004/05/17 18:39:44 $
  */
 public class PoolingDriver implements Driver {
     /** Register an myself with the {@link DriverManager}. */
@@ -53,7 +60,30 @@ public class PoolingDriver implements Driver {
     /** The map of registered pools. */
     protected static HashMap _pools = new HashMap();
 
+    /** Controls access to the underlying connection */
+    private static boolean accessToUnderlyingConnectionAllowed = false; 
+
     public PoolingDriver() {
+    }
+
+    /**
+     * Returns the value of the accessToUnderlyingConnectionAllowed property.
+     * 
+     * @return true if access to the underlying is allowed, false otherwise.
+     */
+    public static synchronized boolean isAccessToUnderlyingConnectionAllowed() {
+        return accessToUnderlyingConnectionAllowed;
+    }
+
+    /**
+     * Sets the value of the accessToUnderlyingConnectionAllowed property.
+     * It controls if the PoolGuard allows access to the underlying connection.
+     * (Default: false)
+     * 
+     * @param allow Access to the underlying connection is granted when true.
+     */
+    public static synchronized void setAccessToUnderlyingConnectionAllowed(boolean allow) {
+        accessToUnderlyingConnectionAllowed = allow;
     }
 
     /**
@@ -142,11 +172,19 @@ public class PoolingDriver implements Driver {
                 throw new SQLException("No pool found for " + url + ".");
             } else {
                 try {
-                    return (Connection)(pool.borrowObject());
+                    Connection conn = (Connection)(pool.borrowObject());
+                    if (conn != null) {
+                        conn = new PoolGuardConnectionWrapper(conn);
+                    } 
+                    return conn;
                 } catch(SQLException e) {
                     throw e;
-                } catch(Throwable e) {
-                    throw new SQLNestedException("Connect failed", e);
+                } catch(NoSuchElementException e) {
+                    throw new SQLNestedException("Cannot get a connection, pool exhausted", e);
+                } catch(RuntimeException e) {
+                    throw e;
+                } catch(Exception e) {
+                    throw new SQLNestedException("Cannot get a connection, general error", e);
                 }
             }
         } else {
@@ -178,4 +216,257 @@ public class PoolingDriver implements Driver {
     protected static int MAJOR_VERSION = 1;
     protected static int MINOR_VERSION = 0;
 
+    /**
+     * PoolGuardConnectionWrapper is a Connection wrapper that makes sure a 
+     * closed connection cannot be used anymore.
+     */
+    private class PoolGuardConnectionWrapper extends DelegatingConnection {
+
+        private Connection delegate;
+    
+        PoolGuardConnectionWrapper(Connection delegate) {
+            super(delegate);
+            this.delegate = delegate;
+        }
+
+        protected void checkOpen() throws SQLException {
+            if(delegate == null) {
+                throw new SQLException("Connection is closed.");
+            }
+        }
+    
+        public void close() throws SQLException {
+            checkOpen();
+            this.delegate.close();
+            this.delegate = null;
+            super.setDelegate(null);
+        }
+
+        public boolean isClosed() throws SQLException {
+            if (delegate == null) {
+                return true;
+            }
+            return delegate.isClosed();
+        }
+
+        public void clearWarnings() throws SQLException {
+            checkOpen();
+            delegate.clearWarnings();
+        }
+
+        public void commit() throws SQLException {
+            checkOpen();
+            delegate.commit();
+        }
+
+        public Statement createStatement() throws SQLException {
+            checkOpen();
+            return delegate.createStatement();
+        }
+
+        public Statement createStatement(int resultSetType, int resultSetConcurrency) throws SQLException {
+            checkOpen();
+            return delegate.createStatement(resultSetType, resultSetConcurrency);
+        }
+
+        public boolean equals(Object obj) {
+            if (delegate == null){
+                return false;
+            }
+            return delegate.equals(obj);
+        }
+
+        public boolean getAutoCommit() throws SQLException {
+            checkOpen();
+            return delegate.getAutoCommit();
+        }
+
+        public String getCatalog() throws SQLException {
+            checkOpen();
+            return delegate.getCatalog();
+        }
+
+        public DatabaseMetaData getMetaData() throws SQLException {
+            checkOpen();
+            return delegate.getMetaData();
+        }
+
+        public int getTransactionIsolation() throws SQLException {
+            checkOpen();
+            return delegate.getTransactionIsolation();
+        }
+
+        public Map getTypeMap() throws SQLException {
+            checkOpen();
+            return delegate.getTypeMap();
+        }
+
+        public SQLWarning getWarnings() throws SQLException {
+            checkOpen();
+            return delegate.getWarnings();
+        }
+
+        public int hashCode() {
+            if (delegate == null){
+                return 0;
+            }
+            return delegate.hashCode();
+        }
+
+        public boolean isReadOnly() throws SQLException {
+            checkOpen();
+            return delegate.isReadOnly();
+        }
+
+        public String nativeSQL(String sql) throws SQLException {
+            checkOpen();
+            return delegate.nativeSQL(sql);
+        }
+
+        public CallableStatement prepareCall(String sql) throws SQLException {
+            checkOpen();
+            return delegate.prepareCall(sql);
+        }
+
+        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+            checkOpen();
+            return delegate.prepareCall(sql, resultSetType, resultSetConcurrency);
+        }
+
+        public PreparedStatement prepareStatement(String sql) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql);
+        }
+
+        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql, resultSetType, resultSetConcurrency);
+        }
+
+        public void rollback() throws SQLException {
+            checkOpen();
+            delegate.rollback();
+        }
+
+        public void setAutoCommit(boolean autoCommit) throws SQLException {
+            checkOpen();
+            delegate.setAutoCommit(autoCommit);
+        }
+
+        public void setCatalog(String catalog) throws SQLException {
+            checkOpen();
+            delegate.setCatalog(catalog);
+        }
+
+        public void setReadOnly(boolean readOnly) throws SQLException {
+            checkOpen();
+            delegate.setReadOnly(readOnly);
+        }
+
+        public void setTransactionIsolation(int level) throws SQLException {
+            checkOpen();
+            delegate.setTransactionIsolation(level);
+        }
+
+        public void setTypeMap(Map map) throws SQLException {
+            checkOpen();
+            delegate.setTypeMap(map);
+        }
+
+        public String toString() {
+            if (delegate == null){
+                return null;
+            }
+            return delegate.toString();
+        }
+
+        // ------------------- JDBC 3.0 -----------------------------------------
+        // Will be commented by the build process on a JDBC 2.0 system
+
+/* JDBC_3_ANT_KEY_BEGIN */
+
+        public int getHoldability() throws SQLException {
+            checkOpen();
+            return delegate.getHoldability();
+        }
+    
+        public void setHoldability(int holdability) throws SQLException {
+            checkOpen();
+            delegate.setHoldability(holdability);
+        }
+
+        public java.sql.Savepoint setSavepoint() throws SQLException {
+            checkOpen();
+            return delegate.setSavepoint();
+        }
+
+        public java.sql.Savepoint setSavepoint(String name) throws SQLException {
+            checkOpen();
+            return delegate.setSavepoint(name);
+        }
+
+        public void releaseSavepoint(java.sql.Savepoint savepoint) throws SQLException {
+            checkOpen();
+            delegate.releaseSavepoint(savepoint);
+        }
+
+        public void rollback(java.sql.Savepoint savepoint) throws SQLException {
+            checkOpen();
+            delegate.rollback(savepoint);
+        }
+
+        public Statement createStatement(int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            checkOpen();
+            return delegate.createStatement(resultSetType, resultSetConcurrency, resultSetHoldability);
+        }
+
+        public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            checkOpen();
+            return delegate.prepareCall(sql, resultSetType, resultSetConcurrency, resultSetHoldability);
+        }
+
+        public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql, autoGeneratedKeys);
+        }
+
+        public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql, resultSetType, resultSetConcurrency, resultSetHoldability);
+        }
+
+        public PreparedStatement prepareStatement(String sql, int[] columnIndexes) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql, columnIndexes);
+        }
+
+        public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
+            checkOpen();
+            return delegate.prepareStatement(sql, columnNames);
+        }
+
+/* JDBC_3_ANT_KEY_END */
+
+        /**
+         * @see org.apache.commons.dbcp.DelegatingConnection#getDelegate()
+         */
+        public Connection getDelegate() {
+            if (isAccessToUnderlyingConnectionAllowed()) {
+                return super.getDelegate();
+            } else {
+                return null;
+            }
+        }
+
+        /**
+         * @see org.apache.commons.dbcp.DelegatingConnection#getInnermostDelegate()
+         */
+        public Connection getInnermostDelegate() {
+            if (isAccessToUnderlyingConnectionAllowed()) {
+                return super.getInnermostDelegate();
+            } else {
+                return null;
+            }
+        }
+    }
 }
