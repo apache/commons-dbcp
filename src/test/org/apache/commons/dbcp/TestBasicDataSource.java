2c00c1e68ac482df6f56351ff75fefbf83be9e6a
Dirk Verbeeck <dirkv@apache.org>
2004-05-18 18:41:16 +0000

    Bugzilla Bug 29054: The BasicDataSource.setTestOnReturn(boolean) is not carried through to the GenericObjectPool variable _testOnReturn. - testcase shows issue is invalid
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@132181 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/dbcp/TestBasicDataSource.java
+++ src/test/org/apache/commons/dbcp/TestBasicDataSource.java
@@ -27,7 +27,7 @@ import junit.framework.TestSuite;
  * TestSuite for BasicDataSource
  * 
  * @author Dirk Verbeeck
- * @version $Revision: 1.18 $ $Date: 2004/05/01 12:54:32 $
+ * @version $Revision: 1.19 $ $Date: 2004/05/18 18:41:16 $
  */
 public class TestBasicDataSource extends TestConnectionPool {
     public TestBasicDataSource(String testName) {
@@ -247,5 +247,24 @@ public class TestBasicDataSource extends TestConnectionPool {
         catch(SQLException ex) { }
         
         assertEquals(0, ds.getNumActive());
+    }
+    
+    /** 
+     * Bugzilla Bug 29054: 
+     * The BasicDataSource.setTestOnReturn(boolean) is not carried through to 
+     * the GenericObjectPool variable _testOnReturn.
+     */ 
+    public void testPropertyTestOnReturn() throws Exception {
+        ds.setValidationQuery("select 1 from dual");
+        ds.setTestOnBorrow(false);
+        ds.setTestWhileIdle(false);
+        ds.setTestOnReturn(true);
+        
+        Connection conn = ds.getConnection();
+        assertNotNull(conn);
+        
+        assertEquals(false, ds.getConnectionPool().getTestOnBorrow());
+        assertEquals(false, ds.getConnectionPool().getTestWhileIdle());
+        assertEquals(true, ds.getConnectionPool().getTestOnReturn());
     }
 }
