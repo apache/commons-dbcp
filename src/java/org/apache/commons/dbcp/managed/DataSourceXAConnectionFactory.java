a0a955d9e4a9fbf0c104cca99e8305f30f6f8873
Phil Steitz <psteitz@apache.org>
2007-07-13 13:10:05 +0000

    Added BasicManagedDataSource, extending BasicDataSource. Also improved extensibility of BasicDataSource by encapsulating methods to create object pool, connection factory and datasource instance previously embedded in createDataSource.
    JIRA: DBCP-230
    Patch provided by Dain Sundstrom
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/dbcp/trunk@555980 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/managed/DataSourceXAConnectionFactory.java
+++ src/java/org/apache/commons/dbcp/managed/DataSourceXAConnectionFactory.java
@@ -33,6 +33,8 @@ import java.sql.SQLException;
 public class DataSourceXAConnectionFactory implements XAConnectionFactory {
     protected TransactionRegistry transactionRegistry;
     protected XADataSource xaDataSource;
+    protected String username;
+    protected String password;
 
     /**
      * Creates an DataSourceXAConnectionFactory which uses the specified XADataSource to create database
@@ -42,11 +44,50 @@ public class DataSourceXAConnectionFactory implements XAConnectionFactory {
      * @param xaDataSource the data source from which connections will be retrieved
      */
     public DataSourceXAConnectionFactory(TransactionManager transactionManager, XADataSource xaDataSource) {
+        this(transactionManager, xaDataSource, null, null);
+    }
+
+    /**
+     * Creates an DataSourceXAConnectionFactory which uses the specified XADataSource to create database
+     * connections.  The connections are enlisted into transactions using the specified transaction manager.
+     *
+     * @param transactionManager the transaction manager in which connections will be enlisted
+     * @param xaDataSource the data source from which connections will be retrieved
+     * @param username the username used for authenticating new connections or null for unauthenticated
+     * @param password the password used for authenticating new connections
+     */
+    public DataSourceXAConnectionFactory(TransactionManager transactionManager, XADataSource xaDataSource, String username, String password) {
         if (transactionManager == null) throw new NullPointerException("transactionManager is null");
         if (xaDataSource == null) throw new NullPointerException("xaDataSource is null");
 
         this.transactionRegistry = new TransactionRegistry(transactionManager);
         this.xaDataSource = xaDataSource;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Gets the username used to authenticate new connections.
+     * @return the user name or null if unauthenticated connections are used
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the username used to authenticate new connections.
+     * @param username the username used for authenticating the connection or null for unauthenticated
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Sets the password used to authenticate new connections.
+     * @param password the password used for authenticating the connection or null for unauthenticated
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public TransactionRegistry getTransactionRegistry() {
@@ -55,7 +96,12 @@ public class DataSourceXAConnectionFactory implements XAConnectionFactory {
 
     public Connection createConnection() throws SQLException {
         // create a new XAConection
-        XAConnection xaConnection = xaDataSource.getXAConnection();
+        XAConnection xaConnection;
+        if (username == null) {
+            xaConnection = xaDataSource.getXAConnection();
+        } else {
+            xaConnection = xaDataSource.getXAConnection(username, password);
+        }
 
         // get the real connection and XAResource from the connection
         Connection connection = xaConnection.getConnection();
