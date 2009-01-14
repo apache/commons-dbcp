7592f687b69bc9e7b90d6ebfd2305a1844deaae1
Mark Emlyn David Thomas <markt@apache.org>
2009-01-14 13:21:46 +0000

    Fix brain dead typo
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@734384 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/cpdsadapter/PooledConnectionImpl.java
+++ src/java/org/apache/commons/dbcp/cpdsadapter/PooledConnectionImpl.java
@@ -26,7 +26,7 @@ import java.util.Vector;
 import javax.sql.ConnectionEvent;
 import javax.sql.ConnectionEventListener;
 import javax.sql.PooledConnection;
-/* JDBC_4_ANT_KEY_START */
+/* JDBC_4_ANT_KEY_BEGIN */
 import javax.sql.StatementEventListener;
 /* JDBC_4_ANT_KEY_END */
 
