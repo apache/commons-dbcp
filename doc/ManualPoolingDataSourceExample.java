5c9d2bf791a1cb7e247ac6c8702938d02d3ce4b8
Dennis Lundberg <dennisl@apache.org>
2009-08-12 12:30:28 +0000

    Update the documentation to use commons-pool-1.5.1 consistently.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@803473 13f79535-47bb-0310-9956-ffa450edef68

--- doc/ManualPoolingDataSourceExample.java
+++ doc/ManualPoolingDataSourceExample.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,13 +50,13 @@ import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 
 //
 // To compile this example, you'll want:
-//  * commons-poo-1.3.jar
+//  * commons-pool-1.5.1.jar
 //  * commons-dbcp-1.2.2.jar
 //  * j2ee.jar (for the javax.sql classes)
 // in your classpath.
 //
 // To run this example, you'll want:
-//  * commons-poo-1.3.jar
+//  * commons-pool-1.5.1.jar
 //  * commons-dbcp-1.2.2.jar
 //  * j2ee.jar (for the javax.sql classes)
 //  * the classes for your (underlying) JDBC driver
