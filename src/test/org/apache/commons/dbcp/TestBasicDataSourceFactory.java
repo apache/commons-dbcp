feedc10e51f313ecba800eb39ad7c04ca142d3e4
Phil Steitz <psteitz@apache.org>
2009-10-22 10:27:43 +0000

    Added validationQueryTimeout to properties set by BasicDataSourceFactory (should have been included in r756628).  Thanks to Roshan Gunasekara. JIRA: DBCP-226.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@828639 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/dbcp/TestBasicDataSourceFactory.java
+++ src/test/org/apache/commons/dbcp/TestBasicDataSourceFactory.java
@@ -17,6 +17,7 @@
 
 package org.apache.commons.dbcp;
 
+import java.sql.Connection;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -53,24 +54,64 @@ public class TestBasicDataSourceFactory extends TestCase {
         properties.setProperty("driverClassName", "org.apache.commons.dbcp.TesterDriver");
         properties.setProperty("url", "jdbc:apache:commons:testdriver");
         properties.setProperty("maxActive", "10");
+        properties.setProperty("maxIdle", "8");
+        properties.setProperty("minIdle", "0");
         properties.setProperty("maxWait", "500");
+        properties.setProperty("initialSize", "5");
         properties.setProperty("defaultAutoCommit", "true");
         properties.setProperty("defaultReadOnly", "false");
         properties.setProperty("defaultTransactionIsolation", "READ_COMMITTED");
         properties.setProperty("defaultCatalog", "test");
+        properties.setProperty("testOnBorrow", "true");
+        properties.setProperty("testOnReturn", "false");
         properties.setProperty("username", "username");
         properties.setProperty("password", "password");
         properties.setProperty("validationQuery", "SELECT DUMMY FROM DUAL");
+        properties.setProperty("validationQueryTimeout", "100");
         properties.setProperty("initConnectionSqls", "SELECT 1;SELECT 2");
+        properties.setProperty("timeBetweenEvictionRunsMillis", "1000");
+        properties.setProperty("minEvictableIdleTimeMillis", "2000");
+        properties.setProperty("numTestsPerEvictionRun", "2");
+        properties.setProperty("testWhileIdle", "true");
+        properties.setProperty("accessToUnderlyingConnectionAllowed", "true");
+        properties.setProperty("removeAbandoned", "true");
+        properties.setProperty("removeAbandonedTimeout", "3000");
+        properties.setProperty("logAbandoned", "true");
+        properties.setProperty("poolPreparedStatements", "true");
+        properties.setProperty("maxOpenPreparedStatements", "10");
 
         BasicDataSource ds = (BasicDataSource) BasicDataSourceFactory.createDataSource(properties);
         
+        assertEquals("org.apache.commons.dbcp.TesterDriver", ds.getDriverClassName());
         assertEquals("jdbc:apache:commons:testdriver", ds.getUrl());
         assertEquals(10, ds.getMaxActive());
+        assertEquals(8, ds.getMaxIdle());
+        assertEquals(0, ds.getMinIdle());
+        assertEquals(500, ds.getMaxWait());
+        assertEquals(5, ds.getInitialSize());
+        assertEquals(5, ds.getNumIdle());
         assertEquals(true, ds.getDefaultAutoCommit());
-
+        assertEquals(false, ds.getDefaultReadOnly());
+        assertEquals(Connection.TRANSACTION_READ_COMMITTED, ds.getDefaultTransactionIsolation());
+        assertEquals("test", ds.getDefaultCatalog());
+        assertEquals(true, ds.getTestOnBorrow());
+        assertEquals(false, ds.getTestOnReturn());
+        assertEquals("username", ds.getUsername());
+        assertEquals("password", ds.getPassword());
+        assertEquals("SELECT DUMMY FROM DUAL", ds.getValidationQuery());
+        assertEquals(100, ds.getValidationQueryTimeout());
         assertEquals(2, ds.connectionInitSqls.size());
         assertEquals("SELECT 1", ds.connectionInitSqls.get(0));
         assertEquals("SELECT 2", ds.connectionInitSqls.get(1));
+        assertEquals(1000, ds.getTimeBetweenEvictionRunsMillis());
+        assertEquals(2000, ds.getMinEvictableIdleTimeMillis());
+        assertEquals(2, ds.getNumTestsPerEvictionRun());
+        assertEquals(true, ds.getTestWhileIdle());
+        assertEquals(true, ds.isAccessToUnderlyingConnectionAllowed());
+        assertEquals(true, ds.getRemoveAbandoned());
+        assertEquals(3000, ds.getRemoveAbandonedTimeout());
+        assertEquals(true, ds.getLogAbandoned());
+        assertEquals(true, ds.isPoolPreparedStatements());
+        assertEquals(10, ds.getMaxOpenPreparedStatements());
     }
 }
