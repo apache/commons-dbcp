19cbaa60d7b6f92f699005ad999188e37adb0e9f
Mark Emlyn David Thomas <markt@apache.org>
2009-10-27 09:55:10 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@830110 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/datasources/KeyedCPDSConnectionFactory.java
+++ src/java/org/apache/commons/dbcp/datasources/KeyedCPDSConnectionFactory.java
@@ -22,7 +22,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.WeakHashMap;
 
