72f39794f7e7063a7e93ae53876d34b3c6ca57d6
Phil Steitz <psteitz@apache.org>
2009-11-21 21:45:58 +0000

    Dropped unused import.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@883005 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/PoolableCallableStatement.java
+++ src/java/org/apache/commons/dbcp/PoolableCallableStatement.java
@@ -19,7 +19,6 @@ package org.apache.commons.dbcp;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
