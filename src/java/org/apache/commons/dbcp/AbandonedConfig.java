d6c75bda8836964c9b714fd697a6d1241f694d82
Phil Steitz <psteitz@apache.org>
2011-04-25 19:25:41 +0000

    Javadoc fixes.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/branches/DBCP_1_4_x_BRANCH@1096570 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp/AbandonedConfig.java
+++ src/java/org/apache/commons/dbcp/AbandonedConfig.java
@@ -80,7 +80,7 @@ public class AbandonedConfig {
      * <p>Abandoned connection cleanup happens when
      * <code><ul>
      * <li><code>{@link #getRemoveAbandoned() removeAbandoned} == true</li>
-     * <li>{@link #getNumIdle() numIdle &lt; 2</li>
+     * <li>{@link #getNumIdle() numIdle} &lt; 2</li>
      * <li>{@link #getNumActive() numActive} &gt; {@link #getMaxActive() maxActive} - 3</li>
      * </ul></code></p>
      * 
