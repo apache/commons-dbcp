b3faaefc13f454e0b382d8026eb8aba8c77d3223
Mark Emlyn David Thomas <markt@apache.org>
2011-08-04 22:01:24 +0000

    Generics
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@1154033 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp2/managed/PoolableManagedConnectionFactory.java
+++ src/java/org/apache/commons/dbcp2/managed/PoolableManagedConnectionFactory.java
@@ -18,6 +18,8 @@
 package org.apache.commons.dbcp2.managed;
 import java.sql.Connection;
 
+import org.apache.commons.dbcp2.DelegatingPreparedStatement;
+import org.apache.commons.dbcp2.PStmtKey;
 import org.apache.commons.dbcp2.PoolableConnection;
 import org.apache.commons.dbcp2.PoolableConnectionFactory;
 import org.apache.commons.dbcp2.PoolingConnection;
@@ -62,15 +64,16 @@ public class PoolableManagedConnectionFactory extends PoolableConnectionFactory 
         initializeConnection(conn);
         if(poolStatements) {
             conn = new PoolingConnection(conn);
-            GenericKeyedObjectPoolConfig config =
-                new GenericKeyedObjectPoolConfig();
+            GenericKeyedObjectPoolConfig<PStmtKey,DelegatingPreparedStatement> config =
+                new GenericKeyedObjectPoolConfig<PStmtKey,DelegatingPreparedStatement>();
             config.setMaxTotalPerKey(-1);
             config.setWhenExhaustedAction(WhenExhaustedAction.FAIL);
             config.setMaxWait(0);
             config.setMaxIdlePerKey(1);
             config.setMaxTotal(maxOpenPreparedStatements);
-            KeyedObjectPool stmtPool =
-                new GenericKeyedObjectPool((PoolingConnection)conn, config);
+            KeyedObjectPool<PStmtKey,DelegatingPreparedStatement> stmtPool =
+                new GenericKeyedObjectPool<PStmtKey,DelegatingPreparedStatement>(
+                        (PoolingConnection)conn, config);
             ((PoolingConnection)conn).setStatementPool(stmtPool);
             ((PoolingConnection) conn).setCacheState(_cacheState);
         }
