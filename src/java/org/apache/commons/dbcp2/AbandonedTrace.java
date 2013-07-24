74f4cb737bc1c57f77bbb29aa139dd186eb14e8e
Mark Emlyn David Thomas <markt@apache.org>
2013-07-24 14:14:34 +0000

    Update for recent changes to pool API
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@1506568 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/dbcp2/AbandonedTrace.java
+++ src/java/org/apache/commons/dbcp2/AbandonedTrace.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ package org.apache.commons.dbcp2;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.pool2.impl.TrackedUse;
+import org.apache.commons.pool2.TrackedUse;
 
 /**
  * Tracks db connection usage for recovering and reporting
@@ -27,7 +27,7 @@ import org.apache.commons.pool2.impl.TrackedUse;
  *
  * The JDBC Connection, Statement, and ResultSet classes
  * extend this class.
- * 
+ *
  * @author Glenn L. Nielsen
  * @version $Revision$ $Date$
  */
@@ -62,7 +62,7 @@ public class AbandonedTrace implements TrackedUse {
      * @param parent AbandonedTrace parent object
      */
     private void init(AbandonedTrace parent) {
-        if (parent != null) {                  
+        if (parent != null) {
             parent.addTrace(this);
         }
     }
