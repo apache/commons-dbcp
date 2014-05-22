eb62cc29d8557d04b2f14e30a024a9c17c3b55dd
Mark Emlyn David Thomas <markt@apache.org>
2014-05-22 12:19:13 +0000

    Order imports
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@1596833 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/dbcp2/DelegatingCallableStatement.java
+++ src/main/java/org/apache/commons/dbcp2/DelegatingCallableStatement.java
@@ -17,24 +17,24 @@
 
 package org.apache.commons.dbcp2;
 
-import java.net.URL;
-import java.sql.CallableStatement;
-import java.math.BigDecimal;
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.util.Map;
-import java.sql.Ref;
-import java.sql.Blob;
-import java.sql.Clob;
-import java.sql.Array;
-import java.util.Calendar;
 import java.io.InputStream;
 import java.io.Reader;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Date;
 import java.sql.NClob;
+import java.sql.Ref;
 import java.sql.RowId;
+import java.sql.SQLException;
 import java.sql.SQLXML;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Map;
 
 /**
  * A base delegating implementation of {@link CallableStatement}.
