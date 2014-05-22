6a0a69b9c2a7e5c0371a25472fcf712bd2a93115
Mark Emlyn David Thomas <markt@apache.org>
2014-05-22 12:22:22 +0000

    Code clean-up. No functional change. Import and modifier order.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/dbcp/trunk@1596835 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/dbcp2/DelegatingPreparedStatement.java
+++ src/main/java/org/apache/commons/dbcp2/DelegatingPreparedStatement.java
@@ -17,25 +17,25 @@
 
 package org.apache.commons.dbcp2;
 
+import java.io.InputStream;
+import java.io.Reader;
 import java.math.BigDecimal;
 import java.sql.Array;
 import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.Date;
+import java.sql.NClob;
 import java.sql.PreparedStatement;
 import java.sql.Ref;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.RowId;
 import java.sql.SQLException;
+import java.sql.SQLXML;
 import java.sql.Statement;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Calendar;
-import java.io.InputStream;
-import java.io.Reader;
-import java.sql.NClob;
-import java.sql.RowId;
-import java.sql.SQLXML;
 
 /**
  * A base delegating implementation of {@link PreparedStatement}.
