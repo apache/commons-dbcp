a6feb941e5ff8fde22865f981d912c6d5f48546e
Gary Gregory <garydgregory@gmail.com>
2018-04-18 16:33:16 -0600

    [DBCP-486] DriverAdapterCPDS.setUser(), setPassword(), and getPooledConnection() with null arguments throw NullPointerExceptions when connection properties are set

--- src/main/java/org/apache/commons/dbcp2/managed/ManagedConnection.java
+++ src/main/java/org/apache/commons/dbcp2/managed/ManagedConnection.java
@@ -159,14 +159,16 @@ public class ManagedConnection<C extends Connection> extends DelegatingConnectio
     @Override
     public void close() throws SQLException {
         if (!isClosedInternal()) {
-            try {
-                // Don't actually close the connection if in a transaction. The
-                // connection will be closed by the transactionComplete method.
-                if (transactionContext == null) {
-                    super.close();
+            synchronized (this) {
+                try {
+                    // Don't actually close the connection if in a transaction. The
+                    // connection will be closed by the transactionComplete method.
+                    if (transactionContext == null) {
+                        super.close();
+                    }
+                } finally {
+                    setClosedInternal(true);
                 }
-            } finally {
-                setClosedInternal(true);
             }
         }
     }
@@ -186,7 +188,9 @@ public class ManagedConnection<C extends Connection> extends DelegatingConnectio
     }
 
     protected void transactionComplete() {
-        transactionContext = null;
+        synchronized (this) {
+            transactionContext = null;
+        }
 
         // If we were using a shared connection, clear the reference now that
         // the transaction has completed
