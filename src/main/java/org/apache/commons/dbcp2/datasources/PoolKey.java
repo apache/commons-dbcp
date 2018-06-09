80d570d77a6f0ff71bea6fddb6519d6bc77f408b
Gary Gregory <garydgregory@gmail.com>
2018-06-09 08:48:29 -0600

    Use Eclipse generated hashCode() and equals(). Better ivar name.

--- src/main/java/org/apache/commons/dbcp2/datasources/PoolKey.java
+++ src/main/java/org/apache/commons/dbcp2/datasources/PoolKey.java
@@ -26,40 +26,56 @@ class PoolKey implements Serializable {
     private static final long serialVersionUID = 2252771047542484533L;
 
     private final String dataSourceName;
-    private final String username;
+    private final String userName;
 
     PoolKey(final String dataSourceName, final String username) {
         this.dataSourceName = dataSourceName;
-        this.username = username;
+        this.userName = username;
     }
 
     @Override
-    public boolean equals(final Object obj) {
-        if (obj instanceof PoolKey) {
-            final PoolKey pk = (PoolKey)obj;
-            return (null == dataSourceName ? null == pk.dataSourceName : dataSourceName.equals(pk.dataSourceName)) &&
-                (null == username ? null == pk.username : username.equals(pk.username));
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
         }
-        return false;
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        PoolKey other = (PoolKey) obj;
+        if (dataSourceName == null) {
+            if (other.dataSourceName != null) {
+                return false;
+            }
+        } else if (!dataSourceName.equals(other.dataSourceName)) {
+            return false;
+        }
+        if (userName == null) {
+            if (other.userName != null) {
+                return false;
+            }
+        } else if (!userName.equals(other.userName)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public int hashCode() {
-        int h = 0;
-        if (dataSourceName != null) {
-            h += dataSourceName.hashCode();
-        }
-        if (username != null) {
-            h = 29 * h + username.hashCode();
-        }
-        return h;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dataSourceName == null) ? 0 : dataSourceName.hashCode());
+        result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+        return result;
     }
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer(50);
         sb.append("PoolKey(");
-        sb.append(username).append(", ").append(dataSourceName);
+        sb.append(userName).append(", ").append(dataSourceName);
         sb.append(')');
         return sb.toString();
     }
