d953fe9a7e677a47668ffcb8bf53acfd7b98d67d
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-06-24 22:16:16 +1200

    DBCP-504: update tests for DelegatingStatement to mock and verify

--- src/test/java/org/apache/commons/dbcp2/TestDelegatingStatement.java
+++ src/test/java/org/apache/commons/dbcp2/TestDelegatingStatement.java
@@ -21,9 +21,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -31,72 +32,492 @@ import java.sql.Statement;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- */
 public class TestDelegatingStatement {
 
     private DelegatingConnection<Connection> conn = null;
     private Connection delegateConn = null;
-    private DelegatingStatement stmt = null;
-    private Statement delegateStmt = null;
+    private Statement obj = null;
+    private DelegatingStatement delegate = null;
 
     @Before
     public void setUp() throws Exception {
         delegateConn = new TesterConnection("test", "test");
-        delegateStmt = new TesterStatement(delegateConn);
         conn = new DelegatingConnection<>(delegateConn);
-        stmt = new DelegatingStatement(conn,delegateStmt);
+        obj = mock(Statement.class);
+        delegate = new DelegatingStatement(conn, obj);
     }
 
     @Test
-    public void testExecuteQueryReturnsNull() throws Exception {
-        assertNull(stmt.executeQuery("null"));
-    }
-
-    @Test
-    public void testGetDelegate() throws Exception {
-        assertEquals(delegateStmt,stmt.getDelegate());
-    }
-
-    @Test
-    public void testCheckOpen() throws Exception {
-        stmt.checkOpen();
-        stmt.close();
+    public void testAddBatchString() throws Exception {
         try {
-            stmt.checkOpen();
-            fail("Expecting SQLException");
-        } catch (final SQLException ex) {
-            // expected
+            delegate.addBatch("foo");
+        } catch (SQLException e) {
         }
+        verify(obj, times(1)).addBatch("foo");
     }
 
     @Test
-    public void testIsWrapperFor() throws Exception {
-        final TesterConnection tstConn = new TesterConnection("test", "test");
-        final TesterStatement tstStmt = new TesterStatementNonWrapping(tstConn);
-        final DelegatingConnection<TesterConnection> dconn = new DelegatingConnection<>(tstConn);
-        final DelegatingStatement stamt = new DelegatingStatement(dconn, tstStmt);
-
-        final Class<?> stmtProxyClass = Proxy.getProxyClass(
-                this.getClass().getClassLoader(),
-                Statement.class);
-
-        assertTrue(stamt.isWrapperFor(DelegatingStatement.class));
-        assertTrue(stamt.isWrapperFor(TesterStatement.class));
-        assertFalse(stamt.isWrapperFor(stmtProxyClass));
-
-        stamt.close();
+    public void testCancel() throws Exception {
+        try {
+            delegate.cancel();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).cancel();
     }
 
-    private static class TesterStatementNonWrapping extends TesterStatement {
-
-        public TesterStatementNonWrapping(final Connection conn) {
-            super(conn);
+    @Test
+    public void testClearBatch() throws Exception {
+        try {
+            delegate.clearBatch();
+        } catch (SQLException e) {
         }
+        verify(obj, times(1)).clearBatch();
+    }
 
-        @Override
-        public boolean isWrapperFor(final Class<?> iface) throws SQLException {
-            return false;
+    @Test
+    public void testClearWarnings() throws Exception {
+        try {
+            delegate.clearWarnings();
+        } catch (SQLException e) {
         }
+        verify(obj, times(1)).clearWarnings();
+    }
+
+    @Test
+    public void testClose() throws Exception {
+        try {
+            delegate.close();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).close();
+    }
+
+    @Test
+    public void testCloseOnCompletion() throws Exception {
+        try {
+            delegate.closeOnCompletion();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).closeOnCompletion();
+    }
+
+    @Test
+    public void testExecuteStringIntegerArray() throws Exception {
+        try {
+            delegate.execute("foo", (int[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).execute("foo", (int[]) null);
+    }
+
+    @Test
+    public void testExecuteString() throws Exception {
+        try {
+            delegate.execute("foo");
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).execute("foo");
+    }
+
+    @Test
+    public void testExecuteStringStringArray() throws Exception {
+        try {
+            delegate.execute("foo", (String[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).execute("foo", (String[]) null);
+    }
+
+    @Test
+    public void testExecuteStringInteger() throws Exception {
+        try {
+            delegate.execute("foo", 1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).execute("foo", 1);
+    }
+
+    @Test
+    public void testExecuteBatch() throws Exception {
+        try {
+            delegate.executeBatch();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeBatch();
+    }
+
+    @Test
+    public void testExecuteLargeBatch() throws Exception {
+        try {
+            delegate.executeLargeBatch();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeLargeBatch();
+    }
+
+    @Test
+    public void testExecuteLargeUpdateStringInteger() throws Exception {
+        try {
+            delegate.executeLargeUpdate("foo", 1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeLargeUpdate("foo", 1);
+    }
+
+    @Test
+    public void testExecuteLargeUpdateStringIntegerArray() throws Exception {
+        try {
+            delegate.executeLargeUpdate("foo", (int[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeLargeUpdate("foo", (int[]) null);
+    }
+
+    @Test
+    public void testExecuteLargeUpdateString() throws Exception {
+        try {
+            delegate.executeLargeUpdate("foo");
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeLargeUpdate("foo");
+    }
+
+    @Test
+    public void testExecuteLargeUpdateStringStringArray() throws Exception {
+        try {
+            delegate.executeLargeUpdate("foo", (String[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeLargeUpdate("foo", (String[]) null);
+    }
+
+    @Test
+    public void testExecuteQueryString() throws Exception {
+        try {
+            delegate.executeQuery("foo");
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeQuery("foo");
+    }
+
+    @Test
+    public void testExecuteUpdateStringIntegerArray() throws Exception {
+        try {
+            delegate.executeUpdate("foo", (int[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeUpdate("foo", (int[]) null);
+    }
+
+    @Test
+    public void testExecuteUpdateStringStringArray() throws Exception {
+        try {
+            delegate.executeUpdate("foo", (String[]) null);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeUpdate("foo", (String[]) null);
+    }
+
+    @Test
+    public void testExecuteUpdateString() throws Exception {
+        try {
+            delegate.executeUpdate("foo");
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeUpdate("foo");
+    }
+
+    @Test
+    public void testExecuteUpdateStringInteger() throws Exception {
+        try {
+            delegate.executeUpdate("foo", 1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).executeUpdate("foo", 1);
+    }
+
+    /**
+     * This method is a bit special, and return the delegate connection, not the
+     * wrapped statement's connection.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetConnection() throws Exception {
+        try {
+            delegate.getConnection();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(0)).getConnection();
+    }
+
+    @Test
+    public void testGetFetchDirection() throws Exception {
+        try {
+            delegate.getFetchDirection();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getFetchDirection();
+    }
+
+    @Test
+    public void testGetFetchSize() throws Exception {
+        try {
+            delegate.getFetchSize();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getFetchSize();
+    }
+
+    @Test
+    public void testGetGeneratedKeys() throws Exception {
+        try {
+            delegate.getGeneratedKeys();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getGeneratedKeys();
+    }
+
+    @Test
+    public void testGetLargeMaxRows() throws Exception {
+        try {
+            delegate.getLargeMaxRows();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getLargeMaxRows();
+    }
+
+    @Test
+    public void testGetLargeUpdateCount() throws Exception {
+        try {
+            delegate.getLargeUpdateCount();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getLargeUpdateCount();
+    }
+
+    @Test
+    public void testGetMaxFieldSize() throws Exception {
+        try {
+            delegate.getMaxFieldSize();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getMaxFieldSize();
+    }
+
+    @Test
+    public void testGetMaxRows() throws Exception {
+        try {
+            delegate.getMaxRows();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getMaxRows();
+    }
+
+    @Test
+    public void testGetMoreResultsInteger() throws Exception {
+        try {
+            delegate.getMoreResults(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getMoreResults(1);
+    }
+
+    @Test
+    public void testGetMoreResults() throws Exception {
+        try {
+            delegate.getMoreResults();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getMoreResults();
+    }
+
+    @Test
+    public void testGetQueryTimeout() throws Exception {
+        try {
+            delegate.getQueryTimeout();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getQueryTimeout();
+    }
+
+    @Test
+    public void testGetResultSet() throws Exception {
+        try {
+            delegate.getResultSet();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getResultSet();
+    }
+
+    @Test
+    public void testGetResultSetConcurrency() throws Exception {
+        try {
+            delegate.getResultSetConcurrency();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getResultSetConcurrency();
+    }
+
+    @Test
+    public void testGetResultSetHoldability() throws Exception {
+        try {
+            delegate.getResultSetHoldability();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getResultSetHoldability();
+    }
+
+    @Test
+    public void testGetResultSetType() throws Exception {
+        try {
+            delegate.getResultSetType();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getResultSetType();
+    }
+
+    @Test
+    public void testGetUpdateCount() throws Exception {
+        try {
+            delegate.getUpdateCount();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getUpdateCount();
+    }
+
+    @Test
+    public void testGetWarnings() throws Exception {
+        try {
+            delegate.getWarnings();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).getWarnings();
+    }
+
+    @Test
+    public void testIsCloseOnCompletion() throws Exception {
+        try {
+            delegate.isCloseOnCompletion();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).isCloseOnCompletion();
+    }
+
+    /**
+     * This method is a bit special, and call isClosed in the delegate object
+     * itself, not in the wrapped statement.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testIsClosed() throws Exception {
+        try {
+            delegate.isClosed();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(0)).isClosed();
+    }
+
+    @Test
+    public void testIsPoolable() throws Exception {
+        try {
+            delegate.isPoolable();
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).isPoolable();
+    }
+
+    @Test
+    public void testSetCursorNameString() throws Exception {
+        try {
+            delegate.setCursorName("foo");
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setCursorName("foo");
+    }
+
+    @Test
+    public void testSetEscapeProcessingBoolean() throws Exception {
+        try {
+            delegate.setEscapeProcessing(Boolean.TRUE);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setEscapeProcessing(Boolean.TRUE);
+    }
+
+    @Test
+    public void testSetFetchDirectionInteger() throws Exception {
+        try {
+            delegate.setFetchDirection(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setFetchDirection(1);
+    }
+
+    @Test
+    public void testSetFetchSizeInteger() throws Exception {
+        try {
+            delegate.setFetchSize(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setFetchSize(1);
+    }
+
+    @Test
+    public void testSetLargeMaxRowsLong() throws Exception {
+        try {
+            delegate.setLargeMaxRows(1l);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setLargeMaxRows(1l);
+    }
+
+    @Test
+    public void testSetMaxFieldSizeInteger() throws Exception {
+        try {
+            delegate.setMaxFieldSize(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setMaxFieldSize(1);
+    }
+
+    @Test
+    public void testSetMaxRowsInteger() throws Exception {
+        try {
+            delegate.setMaxRows(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setMaxRows(1);
+    }
+
+    @Test
+    public void testSetPoolableBoolean() throws Exception {
+        try {
+            delegate.setPoolable(Boolean.TRUE);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setPoolable(Boolean.TRUE);
+    }
+
+    @Test
+    public void testSetQueryTimeoutInteger() throws Exception {
+        try {
+            delegate.setQueryTimeout(1);
+        } catch (SQLException e) {
+        }
+        verify(obj, times(1)).setQueryTimeout(1);
+    }
+
+    @Test
+    public void testWrap() throws SQLException {
+        assertEquals(delegate, delegate.unwrap(Statement.class));
+        assertEquals(delegate, delegate.unwrap(DelegatingStatement.class));
+        assertEquals(obj, delegate.unwrap(obj.getClass()));
+        assertNull(delegate.unwrap(String.class));
+        assertTrue(delegate.isWrapperFor(Statement.class));
+        assertTrue(delegate.isWrapperFor(DelegatingStatement.class));
+        assertTrue(delegate.isWrapperFor(obj.getClass()));
+        assertFalse(delegate.isWrapperFor(String.class));
     }
 }
