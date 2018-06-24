177d6f12913adeaaf052cc2a2b9fb0b76f95e399
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-06-24 22:27:28 +1200

    DBCP-504: add tests for Constants

---
+++ src/test/java/org/apache/commons/dbcp2/TestConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.dbcp2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/**
+ * Tests for Constants.
+ */
+public class TestConstants {
+
+    @Test
+    public void testConstants() {
+        assertNotNull(new Constants());
+        assertEquals(",connectionpool=", Constants.JMX_CONNECTION_POOL_BASE_EXT);
+        assertEquals("connections", Constants.JMX_CONNECTION_POOL_PREFIX);
+        assertEquals(",connectionpool=connections,connection=", Constants.JMX_CONNECTION_BASE_EXT);
+        assertEquals(",connectionpool=connections,connection=", Constants.JMX_STATEMENT_POOL_BASE_EXT);
+        assertEquals(",statementpool=statements", Constants.JMX_STATEMENT_POOL_PREFIX);
+    }
+}
