b133be76375910f1795ff6dc2f0a72f826bbf029
Gary Gregory <gardgregory@gmail.com>
2020-11-20 16:42:46 -0500

    Use Objects.hash().

--- src/main/java/org/apache/commons/dbcp2/datasources/UserPassKey.java
+++ src/main/java/org/apache/commons/dbcp2/datasources/UserPassKey.java
@@ -114,10 +114,7 @@ class UserPassKey implements Serializable {
      */
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((userName == null) ? 0 : userName.hashCode());
-        return result;
+        return Objects.hash(userName);
     }
 
 }
