9ecfb03b37d3c57121b83d6f8ff1c719eae2576b
Gary Gregory <gardgregory@gmail.com>
2020-11-20 16:42:46 -0500

    Use Objects.hash().

--- src/main/java/org/apache/commons/dbcp2/datasources/PoolKey.java
+++ src/main/java/org/apache/commons/dbcp2/datasources/PoolKey.java
@@ -57,11 +57,7 @@ class PoolKey implements Serializable {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((dataSourceName == null) ? 0 : dataSourceName.hashCode());
-        result = prime * result + ((userName == null) ? 0 : userName.hashCode());
-        return result;
+        return Objects.hash(dataSourceName, userName);
     }
 
     @Override
