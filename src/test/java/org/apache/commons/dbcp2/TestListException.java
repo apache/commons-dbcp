9d87c24eee23fbd787e074350af23813c1b93799
Gary Gregory <garydgregory@gmail.com>
2023-08-27 10:59:34 -0400

    Remove @SuppressWarnings

--- src/test/java/org/apache/commons/dbcp2/TestListException.java
+++ src/test/java/org/apache/commons/dbcp2/TestListException.java
@@ -33,9 +33,7 @@ public class TestListException {
 
     @Test
     public void testExceptionList() {
-        @SuppressWarnings("unchecked")
-        final
-        List<Throwable> exceptions = Arrays.asList(new NullPointerException(), new RuntimeException());
+        final List<Throwable> exceptions = Arrays.asList(new NullPointerException(), new RuntimeException());
         final ListException list = new ListException("Internal Error", exceptions);
         assertEquals("Internal Error", list.getMessage());
         assertArrayEquals(exceptions.toArray(), list.getExceptionList().toArray());
