39bfef760102b8fd1517c152c1da350873576fd9
Gary Gregory <garydgregory@gmail.com>
2024-05-03 10:01:35 -0400

    Normalize exception message

--- src/main/java/org/apache/commons/dbcp2/PoolingDataSource.java
+++ src/main/java/org/apache/commons/dbcp2/PoolingDataSource.java
@@ -99,13 +99,12 @@ public class PoolingDataSource<C extends Connection> implements DataSource, Auto
      *            the given connection pool.
      */
     public PoolingDataSource(final ObjectPool<C> pool) {
-        Objects.requireNonNull(pool, "Pool must not be null.");
+        Objects.requireNonNull(pool, "pool");
         this.pool = pool;
         // Verify that pool's factory refers back to it. If not, log a warning and try to fix.
         if (this.pool instanceof GenericObjectPool<?>) {
-            final PoolableConnectionFactory pcf = (PoolableConnectionFactory) ((GenericObjectPool<?>) this.pool)
-                    .getFactory();
-            Objects.requireNonNull(pcf, "PoolableConnectionFactory must not be null.");
+            final PoolableConnectionFactory pcf = (PoolableConnectionFactory) ((GenericObjectPool<?>) this.pool).getFactory();
+            Objects.requireNonNull(pcf, "this.pool.getFactory()");
             if (pcf.getPool() != this.pool) {
                 log.warn(Utils.getMessage("poolingDataSource.factoryConfig"));
                 @SuppressWarnings("unchecked") // PCF must have a pool of PCs
