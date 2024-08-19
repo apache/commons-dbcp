94736302cebadfcf7fd4ef621a9d6ffba4e40cc2
Gary Gregory <garydgregory@gmail.com>
2024-08-19 13:31:17 -0400

    Sort imports

--- src/test/java/org/apache/commons/dbcp2/TestUtils.java
+++ src/test/java/org/apache/commons/dbcp2/TestUtils.java
@@ -23,12 +23,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+
+import org.junit.jupiter.api.Test;
 
 public class TestUtils {
 
