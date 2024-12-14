8b0598a75f029dc3fa05a498b9b1b616e3fda0a0
Gary D. Gregory <garydgregory@gmail.com>
2024-12-14 08:28:56 -0500

    Use JUnit 5's assertThrows()
    Remove unused imports

--- src/test/java/org/apache/commons/dbcp2/datasources/TestPerUserPoolDataSource.java
+++ src/test/java/org/apache/commons/dbcp2/datasources/TestPerUserPoolDataSource.java
@@ -33,7 +33,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
